1f9a6ce5278bff87a7304802e345e3bc274010fd
Maylson <maylsonmoraesd@hotmail.com>
2022-02-05 19:05:15 -0300

    inserindo parametros de rota

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ const express = require("express")
 const mongo = require("mongodb").MongoClient
 
 const app = express()
+app.use(express.json())
 
 app.post("/trip", (req, res) => {
-    /* */
+    const name = req.body.name
+    trips.insertOne({ name: name }, (err, result) => {
+        if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+        }
+        console.log(result)
+        res.status(200).json({ ok: true })
+    })
   })
   app.get("/trips", (req, res) => {
-    /* */
+    trips.find().toArray((err, items) => {
+      if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+      }
+      res.status(200).json({ trips: items })
+    })
   })
   app.post("/expense", (req, res) => {
     /* */
@@ -17,3 +34,23 @@ app.post("/trip", (req, res) => {
   })
 
   app.listen(3000, () => console.log("Server ready"))
+  const url = "mongodb://localhost:27017"
+
+  let db, trips, expenses
+
+mongo.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    db = client.db("tripcost")
+    trips = db.collection("trips")
+    expenses = db.collection("expenses")
+  }
+)
